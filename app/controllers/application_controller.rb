7ac0715ee2d70b19089df23393825b727ce24f2d
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-05 16:08:51 +0900

    エラー対処を元に戻した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      p(p(username == p(ENV["BASIC_AUTH_USER"])) && p(p(password == p(ENV["BASIC_AUTH_PASSWORD"]))
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
   
