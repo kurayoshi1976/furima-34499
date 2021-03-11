3638151b072ad31275664c56d7cbbd37949b93d0
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-11 17:19:37 +0900

    エラーメッセージ設定バリデーション正規表現設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_katakana, presence: true
-  validates :first_name_katakana, presence: true
+  validates :last_name, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
+  validates :first_name, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
