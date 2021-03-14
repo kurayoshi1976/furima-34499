672ca1d5e987684be234438af2869c15e9bf429a
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-14 14:57:06 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,3 +3,20 @@ class Item < ApplicationRecord
   has_one_attached :image
 
 end
+
+with_options presence: true do
+  validates :image
+  validates :name
+  validates :describe
+  validates :category_id
+  validates :item_condition_id
+  validates :shipping_charge_id
+  validates :prefecture_id
+  validates :delivery_time_id
+  validates :price, :format: { with /\A[0-9]+\z/ }, if:
+    :price?
+      def price?
+        if price? >= 300 && price? <= 9999999
+        end
+      end
+end
