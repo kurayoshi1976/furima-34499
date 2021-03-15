96050edb45333231f94e82a45985bd46b2337e9a
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-15 21:38:34 +0900

    ストロングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :describe, :category, :item_condition, :shipping_charge, :prefecture, :delivery_time, :price, :image).(user_id: current_user.id)
   end
 
 end
