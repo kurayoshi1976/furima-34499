8a9e9a2e2d2c66288d9286e66a817791aa5ce790
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-17 11:52:12 +0900

    バリデーション見直し修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   belongs_to :category, :delivery_time, :item_condition, :prefecture, :shipping_charge
 
   validates :item_id, presence: true
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: 'Select' } do
       validates :category_id
       validates :delivery_time_id
       validates :item_condition_id
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :image
-    validates :name
-    validates :describe
+    validates :name, length: {maximum: 40 }
+    validates :describe, length: {maximum: 1000 }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
     validates :item_condition_id
     validates :shipping_charge_id
