a9bc6e54f068bf56f55016299794684ba764f286
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-17 20:15:05 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name               { 'test' }
+    describe           { 'test' }
+    price              { 100 }
+    category_id        { 1 }
+    item_condition_id  { 1 }
+    shipping_charge_id { 1 }
+    prefecture_id      { 1 }
+    delivery_time_id   { 1 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.jpeg'), filename: 'test.jpeg')
+    end
   end
+
 end
