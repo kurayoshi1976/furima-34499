ec3c3b5a74576ac4acb119c5047fa5a32c375f31
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-22 20:40:10 +0900

    編集画面遷移の追加実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_edit, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,16 +21,35 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  
   private
 
   def item_params
     params.require(:item).permit(:name, :describe, :category_id, :item_condition_id, :shipping_charge_id, :prefecture_id,
                                  :delivery_time_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_edit
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
