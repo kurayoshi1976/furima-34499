f4a81431e29f723d259da217bea00018612e4f51
kurayoshi1976 <pa73524@tf6.so-net.ne.jp>
2021-03-31 13:00:24 +0900

    テストコード実装

--- app/models/delivery_purchase.rb
+++ app/models/delivery_purchase.rb
@@ -9,8 +9,7 @@ class DeliveryPurchase
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }  
     validates :municipality
     validates :house_number
-    validates :building
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
   end
   validates :prefecture_id, numericality: { other_than: 1, message: 'select' }
 
---
+++ spec/factories/delivery_purchase.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :delivery_purchase do
+    postal_code    { '111-1111' }
+    prefecture_id   { 2 }
+    municipality    { '某市' }
+    house_number    { '某1丁目' }
+    building        { nil }
+    phone_number    { 11111111111 }
+  end
+end
